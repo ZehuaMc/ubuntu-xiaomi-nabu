10470c76caacbb4f852f417ea9faae11bff54f74
Zehua <39440865+ZehuaMc@users.noreply.github.com>
2025-01-04 23:48:45 +0800

    Update nabu-rootfs_build.sh

--- nabu-rootfs_build.sh
+++ nabu-rootfs_build.sh
@@ -63,11 +63,8 @@ chroot rootdir apt install -y rmtfs protection-domain-mapper tqftpserv
 #Remove check for "*-laptop"
 sed -i '/ConditionKernelVersion/d' rootdir/lib/systemd/system/pd-mapper.service
 
-cp ./*-xiaomi-nabu.deb rootdir/tmp/
 cp ./xiaomi-nabu-debs/*-xiaomi-nabu.deb rootdir/tmp/
-cp /home/runner/work/ubuntu-xiaomi-nabu/ubuntu-xiaomi-nabu/xiaomi-nabu-debs/*-xiaomi-nabu.deb rootdir/tmp/
-cp /home/runner/work/ubuntu-xiaomi-nabu/xiaomi-nabu-debs/*-xiaomi-nabu.deb rootdir/tmp/
-cp /home/runner/work/xiaomi-nabu-debs/*-xiaomi-nabu.deb rootdir/tmp/
+cp ./*-xiaomi-nabu.deb rootdir/tmp/
 
 chroot rootdir dpkg -i /tmp/linux-xiaomi-nabu.deb
 chroot rootdir dpkg -i /tmp/firmware-xiaomi-nabu.deb
